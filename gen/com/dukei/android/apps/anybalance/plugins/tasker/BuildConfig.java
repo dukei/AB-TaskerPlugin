bcdaab048c9693d30400004628faba6c4ac4ee32
VostrikovVA <v.vostrikov@softworks.ru>
2016-03-16 12:50:02 +0300

    Удалены старые исходники

--- gen/com/dukei/android/apps/anybalance/plugins/tasker/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.dukei.android.apps.anybalance.plugins.tasker;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
